748908e06f6878daa60d24a6bf5bd578ad227b24
xueqi <xueqi@guahao.com>
2017-05-06 16:15:35 +0800

    learn Lottie

---
+++ app/src/main/java/com/daystudy/daystudy/Lottie/Lottie2Activity.java
@@ -0,0 +1,89 @@
+package com.daystudy.daystudy.Lottie;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.daystudy.daystudy.R;
+
+public class Lottie2Activity extends AppCompatActivity {
+
+    private int mExtra;
+    private LottieAnimationView mAnimView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottie2);
+        mAnimView = (LottieAnimationView) findViewById(R.id.animation_view);
+        Intent intent = getIntent();
+        if (intent != null) {
+            mExtra = intent.getIntExtra(LottieActivity.WAY_TO_USE,-1);
+            processData();
+        }
+    }
+
+    private void processData() {
+        switch (mExtra) {
+            case 1:
+                mAnimView.setAnimation("lottiefiles.com - Animated Graph.json");
+                mAnimView.loop(true);
+                mAnimView.playAnimation();
+            break;
+            case 2:
+                mAnimView.setAnimation("lottiefiles.com - Countdown.json");
+                mAnimView.loop(true);
+                mAnimView.playAnimation();
+                //mAnimView.setProgress(0.5f);
+                mAnimView.addAnimatorListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Toast.makeText(Lottie2Activity.this, "精美的动画开始了", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Toast.makeText(Lottie2Activity.this, "精美的动画结束了哦~~", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        Toast.makeText(Lottie2Activity.this, "精美的动画被取消了哦~~", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                        Toast.makeText(Lottie2Activity.this, "精美的动画在重复哦~~", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                mAnimView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mAnimView.isAnimating()) {
+                            mAnimView.cancelAnimation();
+                        }else {
+                            //自定义速度
+                            mAnimView.setAnimation("lottiefiles.com - Camera.json");
+                            mAnimView.loop(true);
+                            mAnimView.playAnimation();
+                        }
+
+                    }
+                });
+
+                break;
+            case 3:
+
+
+
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/daystudy/daystudy/Lottie/LottieActivity.java
@@ -0,0 +1,39 @@
+package com.daystudy.daystudy.Lottie;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.daystudy.daystudy.R;
+
+public class LottieActivity extends AppCompatActivity {
+
+    public static final String WAY_TO_USE = "useway";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottie);
+    }
+
+    public void simpleUse(View view){
+        Intent intent = new Intent(this, Lottie2Activity.class);
+        intent.putExtra("useway",-1);
+        startActivity(intent);
+    }
+
+    public void codeUse(View view) {
+        Intent intent = new Intent(this, Lottie2Activity.class);
+        intent.putExtra("useway", 1);
+        startActivity(intent);
+    }
+
+    public void controlUse(View view) {
+        Intent intent = new Intent(this, Lottie2Activity.class);
+        intent.putExtra("useway", 2);
+        startActivity(intent);
+    }
+
+
+}
